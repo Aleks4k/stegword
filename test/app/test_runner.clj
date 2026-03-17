445b4d4f1aa7941437cf5cc923aa49c561db7c92
Aleks4k <84202397+Aleks4k@users.noreply.github.com>
2026-03-17 11:19:12 +0100

    Changed test_runner

--- test/app/test_runner.clj
+++ test/app/test_runner.clj
@@ -6,8 +6,16 @@
             app.stego-test
             app.actions-test))
 (defn -main []
-  (t/run-tests 'app.validation-test
-               'app.crypto-test
-               'app.image-test
-               'app.stego-test
-               'app.actions-test))
+  (let [results (t/run-tests 'app.validation-test
+                             'app.crypto-test
+                             'app.image-test
+                             'app.stego-test
+                             'app.actions-test)
+        failures (+ (:fail results) (:error results))]
+    (if (> failures 0)
+      (do
+        (println "\nTESTS FAILED!")
+        (System/exit 1))
+      (do
+        (println "\nAll tests passed.")
+        (System/exit 0)))))
